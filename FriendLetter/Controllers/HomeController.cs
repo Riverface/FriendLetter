b3d5e8c2d758dadf97ca42cb46a4552229b20359
Akigawa <sonoskeakigawa@gmail.com>
2020-02-12 13:08:48 -0800

    fixed up

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -11,15 +11,12 @@ namespace FriendLetter.Controllers
         [Route("/goodbye")]
         public string Goodbye() { return "Goodbye friend."; }
 
-        [Route("/postcard")]
-        public ActionResult Letter(string recipient, string sender, string noun, string verb, string adjective)
+        [Route("/letter")]
+        public ActionResult Letter(string recipient, string sender)
         {
             LetterVariable myLetterVariable = new LetterVariable();
-            myLetterVariable.Recipient = "Lina";
-            myLetterVariable.Sender = "Jasmine";
-            myLetterVariable.Noun = "Dead Rat";
-            myLetterVariable.Verb = "Yell";
-            myLetterVariable.Adjective = "Angry";
+            myLetterVariable.Recipient = recipient;
+            myLetterVariable.Sender = sender;
             return View(myLetterVariable);
         }
         
--- FriendLetter/Models/LetterVariable.cs
+++ FriendLetter/Models/LetterVariable.cs
@@ -4,8 +4,5 @@ namespace FriendLetter.Models
     {
         public string Recipient { get; set; }
         public string Sender { get; set; }
-        public string Verb {get; set;}
-        public string Noun {get; set;}
-        public string Adjective {get; set;}
     }
 }
